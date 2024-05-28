4c24e4fe0a8531df2129e7fdcc20614346ac083c
TinNanndo <tin.jovanovic003@gmail.com>
2024-05-28 14:33:40 +0200

    jos uvik tin better

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  purge: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", ],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+      extend: {},
   },
   plugins: [],
 }
