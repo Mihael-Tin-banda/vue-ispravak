957053b9bdbb1694584347a252b7d04a94f99d55
TinNanndo <tin.jovanovic003@gmail.com>
2024-06-09 22:32:12 +0200

    da

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/Mihael-Tin-banda.github.io/public' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/public/' : '/'
 });
