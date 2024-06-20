6217b8f10d780619b358aa62ed42e28bc50d7ddd
MihaelHoli <mholi@student.unipu.hr>
2024-06-20 23:38:00 +0200

    index correct redirect URI

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,10 @@ const logger = require('firebase-functions/logger');
 const functions = require('firebase-functions');
 const { OAuth2Client } = require('google-auth-library');
 
-// Ensure you replace 'your-project-id' with your actual Firebase project ID
 const client = new OAuth2Client(
   '786268764964-ls87mqsuecs253qcs9k20cu103o49vsp.apps.googleusercontent.com', // Replace with your client ID
   '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', // Replace with your client secret
-  'https://your-project-id.firebaseapp.com/__/auth/handler' // Redirect URI for Firebase Auth
+  'https://steps-da687.firebaseapp.com/__/auth/handler' // Redirect URI for Firebase Auth
 );
 
 exports.refreshToken = functions.https.onCall(async (data, context) => {
