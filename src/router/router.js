daaa8ff300110b502f4df3ebe95bd305dea32d3f
TinNanndo <tin.jovanovic003@gmail.com>
2024-06-24 17:31:13 +0200

    pokusavan onaj refresh risit

--- src/router/router.js
+++ src/router/router.js
@@ -1,41 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import PachinkoPage from '../views/PachinkoPage.vue';
-import MinesPage from '../views/MinesPage.vue';
 import HomePage from '../components/HomePage.vue';
-import LuckyNumbers from '../views/LuckyNumbers.vue'
-import WordlePage from '../views/WordlePage.vue'
+import Pachinko from '../views/PachinkoPage.vue';
+import Mines from '../views/MinesPage.vue';
+import LuckyNumbers from '../views/LuckyNumbers.vue';
+import WordlePage from '../views/WordlePage.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomePage
-  },
-  {
-    path: '/pachinko',
-    name: 'pachinko',
-    component: PachinkoPage
-  },
-  {
-    path: '/mines',
-    name: 'mines',
-    component: MinesPage
-  },
-  {
-    path: '/lucky',
-    name: 'lucky',
-    component: LuckyNumbers
-  },
-  {
-    path: '/wordle',
-    name: 'wordle',
-    component: WordlePage
-  }
+  { path: '/', name: 'Home', component: HomePage },
+  { path: '/pachinko', name: 'Pachinko', component: Pachinko },
+  { path: '/mines', name: 'Mines', component: Mines },
+  { path: '/lucky', name: 'LuckyNumbers', component: LuckyNumbers },
+  { path: '/wordle', name: 'Wordle', component: WordlePage },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
