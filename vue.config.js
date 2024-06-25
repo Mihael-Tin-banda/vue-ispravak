9ddb47542c9c852b204a3109f9b2572d62547905
TinNanndo <tin.jovanovic003@gmail.com>
2024-06-25 16:23:15 +0200

    a ne znan sta rec

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/Mihael-Tin-banda.github.io/' : '/',
+  outputDir: 'dist',
+  devServer: {
+    historyApiFallback: true,
+  },
   chainWebpack: config => {
     config.module
       .rule('txt')
