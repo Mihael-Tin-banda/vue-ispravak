53f22e0fb59ec6c0fa39f0b3eeb07627aee370b8
TinNanndo <tin.jovanovic003@gmail.com>
2024-06-26 08:33:19 +0200

    ma doda ikonu za wordle

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -61,12 +61,12 @@
         <span class="absolute inset-0 border-2 border-dashed border-color"></span>
         <div class="relative flex h-full transform items-end border-2 border-color bg-color transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
           <div class="p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
-            <img src="../assets/ikone/clover-solid.svg" class="filter-white svg-size" />
+            <img src="../assets/ikone/wordle.svg" class="filter-white svg-size" />
             <h2 class="mt-4 text-xl sm:text-2xl">Wordle</h2>
           </div>
           <div class="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
             <h3 class="mt-4 text-xl sm:text-2xl">Wordle</h3>
-            <p class="mt-4 text-sm sm:text-base">Lucky Numbers are a game of luck</p>
+            <p class="mt-4 text-sm sm:text-base">Wordle ono s ricima da</p>
             <p class="mt-8 font-bold border-color border-2 p-2 border-dashed w-fit hover:bg-red-500">Press</p>
           </div>
         </div>
